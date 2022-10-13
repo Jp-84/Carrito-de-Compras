04d06be4ee8a66b505a5ee1d1a53df97f223c9b9
Jp-84 <pro38jp@gmail.com>
2022-10-13 07:27:33 -0400

    Inicio de Proyecto

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Variables
+
+const   carrito = document.querySelector('#carrito');
+const   contenedor = document.querySelector('#lista-carrito tbody')
+const   vaciarCarritoBtn = document.querySelector('#vaciar-carrito')
+const   listaCarrito = document.querySelector('#lista-curso')
